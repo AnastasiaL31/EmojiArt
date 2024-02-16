9ecc4814c68e3034fa1294aeadf75512f8858f82
Anastasiia Lulakova <anastasiia.lulakova@nure.ua>
2024-02-16 10:03:20 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Anastasia Lulakova on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
